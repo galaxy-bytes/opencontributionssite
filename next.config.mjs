69cc76abc33ca970d98483f5117eba898bc9160a
Rizel Scarlett <blackgirlbytes@github.com>
2023-05-19 22:20:06 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
-const remarkGfm = require('remark-gfm');
-const withPlugins = require('next-compose-plugins');
-const withMdx = require('next-mdx-enhanced');
+import remarkGfm from 'remark-gfm';
+import { withPlugins } from 'next-compose-plugins';
+import withMdx from 'next-mdx-enhanced';
 
-module.exports = withPlugins([
-  withMdx({
-    extension: /\.mdx?$/,
-    options: {
-      remarkPlugins: [remarkGfm],
-    },
-  }),
-], {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  reactStrictMode: true,
-});
+export default withPlugins(
+  [
+    withMdx({
+      extension: /\.mdx?$/,
+      options: {
+        remarkPlugins: [remarkGfm],
+      },
+    }),
+  ],
+  {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    reactStrictMode: true,
+  }
+);
