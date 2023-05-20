e4855e0c094b524f0a1bd68ab09a079cf1bd7bc7
Rizel Scarlett <blackgirlbytes@github.com>
2023-05-19 22:14:20 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
-import nextMdx from "@next/mdx";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-
-const withMDX = nextMdx({
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeAutolinkHeadings, rehypeSlug],
+    remarkPlugins: [require("remark-gfm")],
+    rehypePlugins: [require("rehype-autolink-headings"), require("rehype-slug")],
   },
 });
 
-export default withMDX({
+module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   webpack: (config, { defaultLoaders }) => {
@@ -20,7 +15,7 @@ export default withMDX({
       use: [
         defaultLoaders.babel,
         {
-          loader: '@mdx-js/loader',
+          loader: "@mdx-js/loader",
         },
       ],
     });
