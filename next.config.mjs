801b97785d6a310b99d21c7338ad45c96b3f4dd4
Rizel Scarlett <blackgirlbytes@github.com>
2023-05-19 22:17:00 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,15 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [require("remark-gfm")],
-    rehypePlugins: [require("rehype-autolink-headings"), require("rehype-slug")],
-  },
-});
+const remarkGfm = require('remark-gfm');
+const withPlugins = require('next-compose-plugins');
+const withMdx = require('next-mdx-enhanced');
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+module.exports = withPlugins([
+  withMdx({
+    extension: /\.mdx?$/,
+    options: {
+      remarkPlugins: [remarkGfm],
+    },
+  }),
+], {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
-  webpack: (config, { defaultLoaders }) => {
-    config.module.rules.push({
-      test: /\.mdx$/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: "@mdx-js/loader",
-        },
-      ],
-    });
-
-    return config;
-  },
 });
